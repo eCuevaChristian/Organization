9d9ff484b95f044af5aa21e39580a784969a5f39
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-07-02 04:33:31 -0500

    cierre sesion mi organization 1era parte

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg/MiOrg';
+import Equipo from './componentes/Equipo/Equipo';
 
 function App() {
 
@@ -16,6 +17,11 @@ function App() {
       <Header/>
       {mostrarFormulario ? <Formulario/> : <></>}
       <MiOrg  cambiarMostrar={cambiarMostrar}/>
+      <Equipo equipo="Programación"/>
+      <Equipo equipo="Front End"/>
+      <Equipo equipo="Data Science"/>
+      <Equipo equipo="Devops"/>
+      <Equipo equipo="UX Y Diseño"/>
     </div>
   );
 }
