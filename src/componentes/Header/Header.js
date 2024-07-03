db94163f59f9db5e162eb15c2e91a2c40e815e8a
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-07-03 04:08:18 -0500

    cambio encabezado

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header(){
     return <header className="header">
-        <img src="img/header.png" alt="emcabezado" />
+        <img src="/img/header.png" alt="encabezado" />
     </header>
 
 }
